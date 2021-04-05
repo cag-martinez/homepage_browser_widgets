692504375e806174d8e28d1b7b558464382f8a70
Christian Gomez Martinez <gomez9289@gmail.com>
2021-04-04 23:15:10 -0400

    lowercased on search and now diplaying on the browser

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-import WeatherCard from '../src/components/WeatherCard';
+//import WeatherCard from '../src/components/WeatherCard';
+
+import City from './components/City';
+import {Container} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <WeatherCard 
+    <Container className="App">
+      {/* <WeatherCard 
               dt={1602104400 * 1000}
               temp_min="22.67"
               temp_max="24.39"
               main="Clear"
               humidity="Humid"
-              icon="11d"/>
-    </div>
+              icon="11d"/> */}
+              <City />
+    </Container>
   );
 }
 
--- src/components/City.js
+++ src/components/City.js
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
-import {Row, Col, FormControl, Button} from 'react-bootstrap';
+import React, {useState} from "react";
+import {Row, Col, FormControl, Button} from "react-bootstrap";
 
 const City = () => {
-    const [searchCity, setCity] = useState ('');
-    return <div>
-            {/* heading */}
-        <Row>
-            <col>
-                <h1>Search City</h1>
-            </col>
-        </Row>
-            {/* search form */}
-        <Row>
+  const [city, setCity] = useState("");
+  return (
+    <>
+      {/* heading */}
+      <Row>
+        <Col>
+          <h1>Search City</h1>
+        </Col>
+      </Row>
+      {/* search form */}
+      <Row>
         <Col xs={4} className="text-center">
-            <FormControl
+          <FormControl
             placeholder="Enter City"
             //update the city basedon the users input : second parameter in line 5
-            onChanage={(event) => {setCity(event.target.value)}}
+            onChange={(event) => 
+              setCity(event.target.value)}
             //value of the selected city : first parameter line 5
-            value = {searchCity} />
-            </Col>
-        </Row>
-            {/* button */}
-        <Row>
-            <Col>
-                <Button onClick={onSearch}> Check Weather </Button>
-            </Col>
-        </Row>
-    </div>
-}
+            value={city}
+          />
+        </Col>
+      </Row>
+      {/* button */}
+      <Row>
+        <Col>
+          <Button onClick={{onsearch}}> Check Weather </Button>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default City;
