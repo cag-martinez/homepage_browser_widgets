9e0e09776279d1b377f62343398643fd500f4a61
Christian Gomez Martinez <gomez9289@gmail.com>
2021-04-05 00:10:38 -0400

    on kjey down event for enter key to search, City.js

--- src/components/City.js
+++ src/components/City.js
@@ -1,22 +1,29 @@
-import React, {useState} from "react";
-import {Row, Col, FormControl, Button} from "react-bootstrap";
-import {API_BASE_URL, API_KEY} from "../apis/config";
+import React, { useState } from "react";
+import { Row, Col, FormControl, Button } from "react-bootstrap";
+import { API_BASE_URL, API_KEY } from "../apis/config";
 
 const City = () => {
   const [city, setCity] = useState("");
-
+  // function to fetch data from the API
   const onSearch = () => {
-      fetch (`${API_BASE_URL}/data/2.5/forecast?q=${city}&appid=${API_KEY}`)
+    fetch(`${API_BASE_URL}/data/2.5/forecast?q=${city}&appid=${API_KEY}`)
       .then((response) => response.json())
       .then((result) => console.log(result));
-  }
+  };
+
+  /* on key down event when user presses enter */
+  const onKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      onSearch();
+    }
+  };
 
   return (
     <>
       {/* heading */}
       <Row>
         <Col>
-          <h1>Search City</h1>
+          <h1>Weather in your City</h1>
         </Col>
       </Row>
       {/* search form */}
@@ -25,21 +32,21 @@ const City = () => {
           <FormControl
             placeholder="Enter City"
             //update the city basedon the users input : second parameter in line 5
-            onChange={(event) => 
-              setCity(event.target.value)}
+            onChange={(event) => setCity(event.target.value)}
             //value of the selected city : first parameter line 5
             value={city}
+            // key down event for enter key
+            onKeyDown={onKeyDown}
           />
         </Col>
       </Row>
       {/* button */}
       <Row>
         <Col>
-          <Button onClick={onSearch} >Check Weather</Button>
+          <Button onClick={onSearch}>Check Weather</Button>
         </Col>
-      </Row>  
-  </>
-  
+      </Row>
+    </>
   );
 };
 
