03fbefe2480b64c3a064c407f0a7a455d05e882d
Christian Gomez Martinez <gomez9289@gmail.com>
2021-04-07 19:22:53 -0400

    Card styling

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -5,7 +5,7 @@ const WeatherCard = ({ dt, temp, temp_min, temp_max, main, icon }) => {
   // creating a date object
   const date = new Date(dt);
   return (
-    <Card style={{ width: "10rem" }}>
+    <Card style={{ width: "10rem", height: "450px", padding: "1px", border: "solid .5px", backgroundColor: "gray", borderRadius: "30px"}}>
       <Card.Img
         variant="top"
         // passing icon prop for icon code
